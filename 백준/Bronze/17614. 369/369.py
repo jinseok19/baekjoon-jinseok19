a6df7993353ee4a08065204c643d056f788b1f0d
jinseok kim <121952875+jinseok19@users.noreply.github.com>
2024-03-12 18:39:22 +0900

    [Bronze III] Title: 369, Time: 276 ms, Memory: 110404 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/17614. 369/369.py
+++ 백준/Bronze/17614. 369/369.py
@@ -1,11 +1,13 @@
 import sys
 
-N = int(sys.stdin.readline())
-n_list = [3,6,9]
+N = int(sys.stdin.readline().rstrip())
+n_list = ['3','6','9']
 result = 0
+
 for i in range(1,N+1):
-    str_i = list(map(int,str(i)))
+    str_i = str(i)
     for j in str_i:
         if j in n_list:
             result+=1
+
 print(result)
